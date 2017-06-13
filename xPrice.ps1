fabeefe935719adf792c943044b79115df44d5e5
BOUKEZZOULA Fouzi <fboukezzoula@finaxys.com>
2017-06-13 14:56:41 +0200

    Update xPrice.ps1

--- xPrice.ps1
+++ xPrice.ps1
@@ -1,46 +1,56 @@
 #.\xPricer.ps1 -Purge no -AzureRmResourceGroup xpricerresourcegroup -AzureRmStorageAccount xpricerstorageaccount -AzureRmBatchAccount xpricerbatchaccount -AzureStorageContainer xpricerstoragecontainer -NbrVM 4 -PoolName xpricerpool
-
 param
     (
           [Parameter(Mandatory=$false)]  [String]$Purge ="no", 
-          [Parameter(Mandatory=$false)]  [String]$AzureRmResourceGroup = "xpricerresourcegroup",
-          [Parameter(Mandatory=$false)]  [String]$AzureRmStorageAccount = "xpricerstorageaccount",
-          [Parameter(Mandatory=$false)]  [String]$AzureRmBatchAccount = "xpricerbatchaccount",
-          [Parameter(mandatory=$false)]  [String]$AzureStorageContainer = "xpricerstoragecontainer",
+          [Parameter(Mandatory=$false)]  [String]$AzureRmResourceGroup = "finaxys-poc-batch",
+          [Parameter(Mandatory=$false)]  [String]$AzureRmStorageAccount = "azurestorageaccount",
+          [Parameter(Mandatory=$false)]  [String]$AzureRmBatchAccount = "azurebatchaccount",
+          [Parameter(mandatory=$false)]  [String]$AzureStorageContainer = "azurestoragecontainer",
           [Parameter(mandatory=$false)]  [int]$NbrVM ="4",
-          [Parameter(mandatory=$false)]  [String]$PoolName="xpricerpool"
+          [Parameter(mandatory=$false)]  [String]$PoolName="finaxyspoolname",
+          [Parameter(mandatory=$false)]  [String]$template_json_file="c:\template.json",
+          [Parameter(mandatory=$false)]  [String]$FakeMarketData="C:\Azure\FakeMarketData",
+          [Parameter(mandatory=$false)]  [String]$settings_file="c:\Settings.settings"
+
       )
+
+Set-StrictMode -version 3
+$ErrorActionPreference = "Stop"
 
 # Get-Module PowerShellGet -list | Select-Object Name,Version,Path
 # Install-Module AzureRM -Force
 
 Login-AzureRmAccount
-
-# Warning !!! All ressources will be delete
-Get-AzureRmresourceGroup | Select ResourceGroupName | Remove-AzureRmResourceGroup -Force 
     
 $global:ReturnsxPricerKeys = [System.Collections.ArrayList]@("")
-$template_json_file = "c:\template.json"
        
-if (($Purge -eq "force")) {    
-Remove-AzureRmResourceGroup –Name "$AzureRmResourceGroup" -Force         
+if (($Purge -eq "yes") -or ($Purge -eq "YES") -or ($Purge -eq "Yes")) {    
+    # Warning !!! All ressources will be delete
+    #Get-AzureRmresourceGroup | Select ResourceGroupName | Remove-AzureRmResourceGroup -Force
+    Remove-AzureRmResourceGroup -Name "$AzureRmResourceGroup" -Force        
 }     
 
 # create a resource Group
 New-AzureRmResourceGroup –Name "$AzureRmResourceGroup" –Location “West Europe” 
 
 # create storage account
-New-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" –Location "West Europe" –Type "Standard_LRS" 
+New-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" –Location "West Europe" –Type "Standard_LRS"
+
+$AzureRmStorageAccountID = Get-AzureRmStorageAccount –ResourceGroup "$AzureRmResourceGroup" –StorageAccountName "$AzureRmStorageAccount" 
 
 # create batch service
-New-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount" –Location "Central US" –ResourceGroupName "$AzureRmResourceGroup"
+New-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount" –Location "West Europe" –ResourceGroupName "$AzureRmResourceGroup" -AutoStorageAccountId $AzureRmStorageAccountID.Id
+
 
 # keys taken from the new batch account created
 $Account = Get-AzureRmBatchAccountKeys –AccountName "$AzureRmBatchAccount"
 $PrimaryAccountKey = $Account.PrimaryAccountKey
 $SecondaryAccountKey = $Account.SecondaryAccountKey 
-$global:ReturnsxPricerKeys.Add("$PrimaryAccountKey")
-$global:ReturnsxPricerKeys.Add("$SecondaryAccountKey")
+
+$BatchAccountKey = $PrimaryAccountKey
+
+$AccountStorage = Get-AzureRmStorageAccountKey –AccountName "$AzureRmStorageAccount" -ResourceGroupName "$AzureRmResourceGroup"
+$AccountStorageKey = $AccountStorage.Value[0]
 
 # variable for the service batch address URL
 $ServiceBatchURL = Get-AzureRmBatchAccount –AccountName "$AzureRmBatchAccount"
@@ -50,13 +60,17 @@ $BatchURL = $ServiceBatchURL.TaskTenantUrl
 $context = Get-AzureRmBatchAccountKeys -AccountName "$AzureRmBatchAccount"
 $configuration = New-Object -TypeName "Microsoft.Azure.Commands.Batch.Models.PSCloudServiceConfiguration" -ArgumentList @(4,"*")
 New-AzureBatchPool -Id "$PoolName" -VirtualMachineSize "Small" -CloudServiceConfiguration $configuration -AutoScaleFormula '$TargetDedicated=4;' -BatchContext $context
-
-$AzureStorageContainerContext = New-AzureStorageContext -StorageAccountName "$AzureRmStorageAccount" -StorageAccountKey $ReturnsxPricerKeys[1] 
-
-New-AzureStorageContainer -Name "$AzureStorageContainer" -Context $AzureStorageContainerContext
 Write-Host "Pool $PoolName has been created ..."
 
-# write all these inforamtion to a json file
+$AzureStorageContainerContext = New-AzureStorageContext -ConnectionString "DefaultEndpointsProtocol=https;AccountName=$AzureRmStorageAccount;AccountKey=$AccountStorageKey;"
+$AzureStorageContainerContext
+
+New-AzureStorageContainer -Name $AzureStorageContainer -Context $AzureStorageContainerContext
+
+# Upload FakeMarketData files 
+ls $FakeMarketData | Set-AzureStorageBlobContent –Container $AzureStorageContainer -Context $AzureStorageContainerContext –ConcurrentTaskCount 16
+
+# write all these information to a json file
 $json = @"
 {
    
@@ -66,7 +80,7 @@ $json = @"
 $jobj = ConvertFrom-Json -InputObject $json    
     
 $jobj | add-member "AzureRmStorageAccount" "$AzureRmStorageAccount" -MemberType NoteProperty
-$jobj | add-member "PrivateKey" "$Key" -MemberType NoteProperty
+$jobj | add-member "PrivateKey" "$BatchAccountKey" -MemberType NoteProperty
 $jobj | add-member "AzureRmBatchAccount" "$AzureRmBatchAccount" -MemberType NoteProperty
 $jobj | add-member "ServiceBatchURL" "$BatchURL" -MemberType NoteProperty
 $jobj | add-member "PoolName" "$PoolName" -MemberType NoteProperty
@@ -74,3 +88,50 @@ $jobj | add-member "AzureStorageContainer" "$AzureStorageContainer" -MemberType 
    
 ConvertTo-Json $jobj | Out-File $template_json_file
 (Get-Content -path "$template_json_file" -Encoding Unicode) | Set-Content -Encoding "Default" -Path "$template_json_file"
+
+# write all these information to a json file
+
+$settings = @"
+{
+<?xml version='1.0' encoding='utf-8'?>
+<SettingsFile xmlns="http://schemas.microsoft.com/VisualStudio/2004/01/settings" CurrentProfile="(Default)" GeneratedClassNamespace="XPricer.Scheduler" GeneratedClassName="Settings">
+  <Profiles />
+  <Settings>
+    <Setting Name="$BatchURL" Type="System.String" Scope="User">
+      <Value Profile="(Default)">https://(YourAccount).(region).batch.azure.com</Value>
+    </Setting>
+    <Setting Name="$AzureRmBatchAccount" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+    <Setting Name="$BatchAccountKey" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+    <Setting Name="StorageServiceUrl" Type="System.String" Scope="User">
+      <Value Profile="(Default)">core.windows.net</Value>
+    </Setting>
+    <Setting Name="$AzureRmStorageAccount" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+    <Setting Name="$AccountStorageKey" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+    <Setting Name="$AzureStorageContainer" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+
+     <Setting Name="$PoolName" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+
+    <Setting Name="ApplicationPackageName" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+    </Setting>
+    <Setting Name="ApplicationPackageVersion" Type="System.String" Scope="User">
+      <Value Profile="(Default)" />
+       </Setting>
+ 
+</SettingsFile>
+"@
+
+$settings | Out-File $settings_file
+(Get-Content -path "$settings_file" -Encoding Unicode) | Set-Content -Encoding "Default" -Path "$settings_file" 
